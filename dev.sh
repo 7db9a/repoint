ba03f33be9d706c1e4b456c631d87d6be4e16f99
7db9a <repo.7db9a@gmail>
2020-07-11 14:38:44 -0400

    chore(dev): rename cli-starter => repoint.

--- dev.sh
+++ dev.sh
@@ -7,15 +7,15 @@ start-docker() {
 cargo-build() {
     docker exec \
     -it \
-    cli-starter_coding_1 \
+    repoint_coding_1 \
     cargo build
 }
 
 cargo-bin-build() {
     docker exec \
     -it \
-    cli-starter_coding_1 \
-    cargo build --bin cli-starter
+    repoint_coding_1 \
+    cargo build --bin repoint
 }
 
 build() {
@@ -26,14 +26,14 @@ build() {
 rust-lib-test() {
     docker exec \
     -it \
-    cli-starter_coding_1 \
+    repoint_coding_1 \
     cargo test $1 -- --test-threads=1 --nocapture
 }
 
 rust-bin-test() {
     docker exec \
     -it \
-    cli-starter_coding_1 \
+    repoint_coding_1 \
     cargo test --bin $1
 }
 
