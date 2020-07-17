1e5c4e50997359704d816a026f5cf21c5e77303d
7db9a <repo.7db9a@gmail>
2020-07-17 15:11:03 -0400

    Saving progress.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -156,6 +156,8 @@ fn init_action(c: &Context) {
 
     if let Some(opreturn_script_path) = c.string_flag("opreturn-script-path") {
         println!("opreturn_script-path: {:?}", opreturn_script_path);
+        repoint::init(opreturn_script_path);
+
     }
     println!("{:?}", pathbuf);
     File::create(&pathbuf).expect("Failed to create file.");
--- src/mod.rs
+++ src/mod.rs
@@ -1,14 +1,17 @@
 pub mod repoint_file;
 use std::process::Command;
+use cmd_lib::run_fun;
 
-pub fn init(cmd: String, args: Vec<String>)/* -> Result<std::process::Output, std::io::Error>*/ {
-    let output_res = Command::new(cmd)
-        .args(args)
-        .output();
+pub fn init(cmd: String)/* -> Result<std::process::Output, std::io::Error>*/ {
+    let output = run_fun!("{}", cmd).unwrap();
+    //let output_res = Command::new("bash")
+    //    .arg("opreturn-script.sh")
+    //    .output();
 
-    let output = output_res.unwrap();
+    //let output = output_res.unwrap();
 
-    println!("results:\n {}", String::from_utf8_lossy(&output.stdout));
+    //println!("results:\n {}", String::from_utf8_lossy(&output.stdout));
+    println!("results:\n {}", output);
 }
 
 pub fn send_opreturn(test: bool) {
