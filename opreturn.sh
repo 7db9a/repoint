4231285f112771d8399eff5f9fae13efc2a536d9
7db9a <repo.7db9a@gmail>
2020-07-20 12:49:45 -0400

    chore: opreturn script.

---
+++ opreturn.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 1 = opcode
+
+docker run --rm repoint_opreturn:0.1.0 node ./lib/opreturn.js \
+5JZ4RXH4MoXpaUQMcJHo8DxhZtkf5U5VnYd9zZH8BRKZuAbxZEw \
+$1 \
+'hello repoint app' \
+400 \
+https://api.mattercloud.net \
+true
