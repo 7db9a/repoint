731cc244b63d77b186468267663eb3c7efc48b82
7db9a <repo.7db9a@gmail>
2020-07-31 07:35:42 -0400

    Saving progress.

--- opreturn.sh
+++ opreturn.sh
@@ -5,8 +5,8 @@
 # 2 = msg
 
 docker run --rm repoint_opreturn:0.1.0 node ./lib/opreturn.js \
-$1 \
-$2
+"5JZ4RXH4MoXpaUQMcJHo8DxhZtkf5U5VnYd9zZH8BRKZuAbxZEw" \
+$2 \
 "$3" \
 400 \
 https://api.mattercloud.net \
